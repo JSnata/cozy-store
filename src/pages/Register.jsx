4f263bc687d58e1c329e44e6fcb41b86c64aa3fb
JSNata <n.kulikouskaya@gmail.com>
2024-12-09 22:15:44 +0100

    refactor: remove test data

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -23,9 +23,9 @@ const Register = () => {
     <section className='h-screen grid place-items-center'>
         <Form method='post' className='card w-96 p-8 bg-base-100 shadow-lg flex flrx-col gap-y-4'>
             <h4 className='text-center text-3xl font-bold'>Register</h4>
-            <FormInput type='text' label='Username' name='username' defaultValue="nata" />
-            <FormInput type='email' label='Email' name='email' defaultValue="nata@gmail.com" />
-            <FormInput type='password' label='Password' name="password" defaultValue="password"/>
+            <FormInput type='text' label='Username' name='username' />
+            <FormInput type='email' label='Email' name='email' />
+            <FormInput type='password' label='Password' name="password" />
             <div className="mt-4 ">
                 <SubmitButton text='Register' />
             </div>
